8c35ed32af0d69c5760928e652980eec316eb57b
TandRAF <grosu.rafael@mail.ru>
2023-04-03 16:48:59 +0300

    acces problem

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename = "/homelab">
-    <App />
+    {/* <App /> */}
     </BrowserRouter>
   </React.StrictMode>
 );
